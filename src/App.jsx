2548c6e7659973608b2097420130ad3d985e223d
Abdisalam Ali <abdisharifali@yahoo.com>
2025-03-17 00:47:22 -0500

    Adding new form

--- src/App.jsx
+++ src/App.jsx
@@ -884,7 +884,7 @@ const LandingPage = () => {
 
       {/* Footer */}
       <footer className="w-full py-6 bg-orange-950 text-center text-sm text-orange-300">
-        &copy; {new Date().getFullYear()} Journa Media. All rights reserved.
+        &copy; {new Date().getFullYear()} Journa Media Company. All rights reserved.
       </footer>
     </div>
   );
